89592a9f6dae41a2211a730a0823433262e4b4d8
pedroramin <pedro.mauro@sou.fafram.com.br>
2026-04-18 16:02:39 +0000

    Add Next.js home page

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: 'Arial, sans-serif', padding: 24 }}>
+      <h1>CV Analyzer</h1>
+      <p>Projeto online.</p>
+    </main>
+  );
+}
